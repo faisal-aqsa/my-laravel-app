d1c3f6890058730d4817db0bd3d5b3eb93a29084
fdnectar <khanfaisal8898@gmail.com>
2026-02-08 16:11:04 +0530

    add

--- resources/views/back/pdf/invoice-pdf.blade.php
+++ resources/views/back/pdf/invoice-pdf.blade.php
@@ -426,7 +426,7 @@
                     @if($logoSrc)
                         <img src="{{ $logoSrc }}" alt="Company Logo" style="width:200px; height:200px; object-fit:contain;">
                         <p class="tag-line">
-                            We Generally Recycle 
+                            We Generally Recycle &nbsp;
                             <i class="fas fa-recycle" style="color:black; font-weight:900;"></i>
                         </p>
                     @endif
