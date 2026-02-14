5450933cd6d7b05df65d599eb4a4ff9df28bfcd7
fdnectar <khanfaisal8898@gmail.com>
2026-02-14 11:49:02 +0530

    add

--- resources/views/back/pdf/delivery-challan-pdf.blade.php
+++ resources/views/back/pdf/delivery-challan-pdf.blade.php
@@ -387,10 +387,22 @@
                                 <td class="value-cell">{{ $challan->challan_number }}</td>
                             </tr>
                             @if($challan->vehicle_no)
-                            <tr>
-                                <td class="label-cell">Vehicle No:</td>
-                                <td class="value-cell">{{ $challan->vehicle_no }}</td>
-                            </tr>
+                                <tr>
+                                    <td class="label-cell">Vehicle No:</td>
+                                    <td class="value-cell">{{ $challan->vehicle_no }}</td>
+                                </tr>
+                            @endif
+                            @if($challan->vehicle_no)
+                                <tr style="display: none;">
+                                    <td class="label-cell">Vehicle No:</td>
+                                    <td class="value-cell">{{ $challan->vehicle_no }}</td>
+                                </tr>
+                            @endif
+                            @if($challan->vehicle_no)
+                                <tr style="display: none;">
+                                    <td class="label-cell">Vehicle No:</td>
+                                    <td class="value-cell">{{ $challan->vehicle_no }}</td>
+                                </tr>
                             @endif
                         </table>
                     </td>
