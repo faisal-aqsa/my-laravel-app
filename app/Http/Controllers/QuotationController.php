b28c622d9608b7a319c34c721bfbdb18f099a492
Faisal Khan <93912781+faisal-aqsa@users.noreply.github.com>
2026-07-07 20:50:15 +0530

    update desing

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -303,7 +303,7 @@ class QuotationController extends Controller
         $quotation = Quotation::with(['items', 'client'])->findOrFail($id);
         $settings = Setting::first();
         
-        $pdf = SnappyPdf::loadView('back.pdf.quotation-pdf', compact('quotation', 'settings'))
+        $pdf = SnappyPdf::loadView('back.pdf.quotation-pdf-2', compact('quotation', 'settings'))
                 ->setOption('enable-local-file-access', true)
                 ->setOption('margin-top', 0)
                 ->setOption('margin-bottom', 0)
